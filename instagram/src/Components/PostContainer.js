7a5e09db7bb0f121b71df57a2aedacc95bb66c0c
Francis Chen <fncis.chen@gmail.com>
2019-02-18 13:55:46 -0800

    psuedocode for post container

--- instagram/src/Components/PostContainer.js
+++ instagram/src/Components/PostContainer.js
@@ -1,1 +1,37 @@
 import React, { Component } from 'react';
+
+class PostContainer extends Component {
+
+        
+    constructor(props) {
+        super(props);
+
+        // properties that will be immutable
+
+            // image 
+            // logo
+            // name of instagram account
+
+            // post description
+
+        // state data that will change include
+        
+            // amount of comments
+            // like button / comment button 
+
+            // amount of likes 
+            
+            // status of recent comment 
+
+        this.state = {
+
+        }
+
+    }
+
+        render() {
+             
+            
+        }
+    
+}
