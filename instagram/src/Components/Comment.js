ce2f55cd0aace88d15500f825334fa9e7473dfe7
Francis Chen <fncis.chen@gmail.com>
2019-02-18 15:51:00 -0800

    added proptypes to comment

--- instagram/src/Components/Comment.js
+++ instagram/src/Components/Comment.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 const Comment = props => {
     return <div className="comment"><b>{props.posterName}:</b>{props.commented}</div>;
