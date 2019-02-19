3ebb16bc6fea17235647a293cae879cb7e463320
Francis Chen <fncis.chen@gmail.com>
2019-02-18 16:05:21 -0800

    updade comment proptypes for comment component

--- instagram/src/Components/Comment.js
+++ instagram/src/Components/Comment.js
@@ -5,5 +5,11 @@ const Comment = props => {
     return <div className="comment"><b>{props.posterName}:</b>{props.commented}</div>;
 }
 
+// proptype => act as a gatekeeper or filter that will show up on console
+// https://labnotes.panderalabs.com/shaping-up-your-react-components-with-proptypes-shape-and-selectors-9d1111d0566f
+
+Comment.propTypes = {
+    isName = props.posterName.
+}
 
 export default Comment; 
