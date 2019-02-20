be8e172e99c80221ec51216447fd4738a06edd0b
Francis Chen <fncis.chen@gmail.com>
2019-02-20 00:09:10 -0800

    added methods for adding comments and relevant event handlers

--- instagram/src/Components/CommentSection.js
+++ instagram/src/Components/CommentSection.js
@@ -6,14 +6,48 @@ class CommentSection extends Component {
 
     constructor(props) {
         super(props);
+    
+        this.state = {
+            comments: []
+
+        }
     }
 
+    componentDidMount() {
+        this.setState({
+            comments: this.props.comments
+        })
+    }
+
+    addComment = e => {
+
+        e.preventDefault();
+
+        this.setState({
+            comments: [..comments ]
+        })
+    }
+
+    handleChanges = e => {
+        console.log(e.target.value);
+        //     this.setState({ name: e.target.value })
+        this.setState({
+          [e.target.name]: e.target.value
+    })
+    };
+
+
+    // https://reactjs.org/docs/forms.html
     render() {
         return (
             <div className="comment-section">
-            {this.props.comments.map(comment => 
+            {this.state.comments.map(comment => 
                 <Comment posterName={comment.username} commented={comment.text} />
             )}
+
+            <form>
+            <input type="submit" maxLength="1000" onChange={handleChanges}></input>
+            </form>
             </div>
         );
     }
