1b86760d9ece52775dc83d995a6dd1e1c7b9997e
Francis Chen <fncis.chen@gmail.com>
2019-02-20 21:51:55 -0800

    updated Authenticate HOC to check if user is logged in

--- instagram/src/Components/Authetnication/Authenticate.js
+++ instagram/src/Components/Authetnication/Authenticate.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+// currying
+// https://blog.bitsrc.io/understanding-currying-in-javascript-ceb2188c339
+
+
+
 const Authenticate = App => LogIn =>  {
     class extends React.Component {
     // leverage conditional rendering 
     // https://reactjs.org/docs/conditional-rendering.html
-    render() {
-        if (localStorage.getItem("username") && localStorage.getItem("password")) {
-            return <App/>
-        }
-        
-        return <LogIn/>
+    constructor() {
+        super();
+
+        this.state = {
+            loggedIn: false
         }
     }
+
+    // great place to check local storage!
+    componentDidMount() {
+        if (localStorage.getItem("username") && localStorage.getItem("password")) {
+            this.setState({loggedIn: true})
+        }
+    }
+
+    render() {
+        if (loggedIn) {
+            return <App/>
+        }
+        else {
+            return <LogIn/>
+        }
+    }
+}
 }
 
 export default Authenticate;
